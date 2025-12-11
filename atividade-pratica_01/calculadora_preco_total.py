45262ed2a2b60bc414880e1ee53ed474a801747d
Ernani Gomes <lic.fisica.cederj@gmail.com>
2025-12-11 11:00:22 -0300

    add atividade prática 01

---
+++ atividade-pratica_01/calculadora_preco_total.py
@@ -0,0 +1,20 @@
+# Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+# Nome do produto: "Cadeira Infantil"
+# Preço unitário: R$ 12.40
+# Quantidade: 3
+# O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+
+# Informações do produto
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = int(input("Insira a quantidade, por favor: "))
+
+# Cálculo do preço total
+preco_total = preco_unitario * quantidade
+
+# Exibição das informações
+print("Produto:", nome_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("Preço total: R$", round(preco_total, 2))
---
+++ atividade-pratica_01/calculadora_soma.py
@@ -0,0 +1,11 @@
+# Desenvolva um programa que soma dois números. Use as variáveis numero1 = 12 e numero2 = 14. O programa deve calcular a soma e exibir o resultado.
+
+# Definindo as variáveis
+numero1 = 12
+numero2 = 14
+
+# Calculando a soma
+soma = numero1 + numero2
+
+# Exibindo o resultado
+print("O resultado da soma entre os números", numero1, "e", numero2, "é:", soma)
